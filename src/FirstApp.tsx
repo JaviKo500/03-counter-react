e8a079da9c074a449e739cf8d5bb715fc075da30
JaViKo500 <javikogutierrez64@gmail.com>
2025-04-11 18:16:17 -0500

    feat(ui): print variables in html

--- src/FirstApp.tsx
+++ src/FirstApp.tsx
@@ -1,11 +1,29 @@
+const person = {
+  name: 'Javiko500',
+  age: 30,
+  city: 'Santiago',
+};
+
+const currentYear = () => {
+  return new Date().getFullYear();
+}
+let counter = 0;
+const add = () => {
+  return counter++;
+}
 
 export const FirstApp = () => {
   return (
     <>
       <h1>
-        first App Javiko500
+        Hello
       </h1>
-      <p>I'm subtitle</p>
+      <p>I'm { person.name }</p>
+      <p>My age is { person.age } in { currentYear() }</p>
+      <p>I live in { person.city }</p>
+      <button onClick={ () => add() }>Click me { counter }</button>
+      <br />
+      <code>{ JSON.stringify(person) }</code>
     </>
   );
 }
