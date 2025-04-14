7c3c08666afe11f324b852f5dda2832184162cfa
JaViKo500 <javikogutierrez64@gmail.com>
2025-04-13 21:19:58 -0500

    feat: add counter component

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
-import { App } from './App';
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
-import { FirstApp } from './FirstApp';
+
+// import { App } from './App';
+// import { FirstApp } from './FirstApp';
+import { CounterApp } from './CounterApp';
 import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
-    <App />
-    <FirstApp title="Hello World" subtitle={'test subtitle'} showTitle />
+    {/* <App /> */}
+    {/* <FirstApp title="Hello World" subtitle={'test subtitle'} showTitle /> */}
+    <CounterApp value={ 10 } />
   </StrictMode>
 );
