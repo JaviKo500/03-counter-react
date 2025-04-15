7ff3462a7bc8d50322db552134a17fcbcc87042e
JaViKo500 <javikogutierrez64@gmail.com>
2025-04-14 22:35:24 -0500

    feat(hook): clic button

--- src/CounterApp.tsx
+++ src/CounterApp.tsx
@@ -2,11 +2,24 @@ interface Props {
   value: number;
 }
 
+const handleAdd = () => {
+  console.log('<--------------- JK CounterApp --------------->');
+  console.log('handleAdd');
+}
+
 export const CounterApp = ( { value } : Props ) => {
   return (
     <>
       <h1>Counter App</h1>
       <h2>{ value }</h2>
+      <button
+        onClick={ handleAdd }
+      >
+        +1
+      </button>
+      <button>
+        -1
+      </button>
     </>
   )
 }
