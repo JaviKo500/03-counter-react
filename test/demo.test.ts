72d74ce0999c98438b61b9a45131a2a3d1a95a00
JaViKo500 <javikogutierrez64@gmail.com>
2025-04-17 10:16:33 -0500

    test: config global jest

--- test/demo.test.ts
+++ test/demo.test.ts
@@ -1,4 +1,8 @@
-import { test} from '@jest/globals';
-test( 'this test should pass', () => {
-
+describe('Demo.test', () => {
+  test( 'this test should pass', () => {
+    const message1 = 'Hello World';
+    const message2 = message1.trim();
+  
+    expect( message1 ).toBe( message2 );
+  });
 });
