bcd412cfa3ab8e1bb8224c57ff4a767f8ff84c1c
JaViKo500 <javikogutierrez64@gmail.com>
2025-05-05 22:06:05 -0500

    test: add testing library react

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,14 @@ import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 
 // import { App } from './App';
-// import { FirstApp } from './FirstApp';
-import { CounterApp } from './CounterApp';
+import { FirstApp } from './FirstApp';
+// import { CounterApp } from './CounterApp';
 import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
     {/* <App /> */}
-    {/* <FirstApp title="Hello World" subtitle={'test subtitle'} showTitle /> */}
-    <CounterApp value={ 10 } />
+    <FirstApp title="Hello World" subtitle={'test subtitle'} showTitle />
+    {/* <CounterApp value={ 10 } /> */}
   </StrictMode>
 );
