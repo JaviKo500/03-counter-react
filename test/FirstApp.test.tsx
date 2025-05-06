070c20e476f984bf81e75cf51737116c7307343b
JaViKo500 <javikogutierrez64@gmail.com>
2025-05-05 22:44:20 -0500

    test: snapshot and get elements

--- test/FirstApp.test.tsx
+++ test/FirstApp.test.tsx
@@ -5,6 +5,17 @@ import React from "react";
 describe('FirstApp.test', () => {
   test( 'check match with snapshot', () => {
     const title = 'Hello World';
-    render(<FirstApp title={title} showTitle/>);
+    const { container } = render(<FirstApp title={title} showTitle/>);
+    expect(container).toMatchSnapshot();
+  });
+
+  test( 'show title in h1', () => {
+    const title = 'Hello World';
+    const { getByText, container } = render(<FirstApp title={title} showTitle/>);
+    const text = getByText(title)
+    expect(text).toBeTruthy();
+
+    const h1 = container.querySelector('h1');
+    expect(h1?.innerHTML).toBe(title);
   });
 }); 
